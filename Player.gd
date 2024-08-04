748ef0a0f56a45d5eecb788e5eeda58edb6c7e9b
guywiththenose <sammylemonmoore@gmail.com>
2024-08-05 10:32:58 +1200

    v1.16
    singletons and knife qol

--- Player.gd
+++ Player.gd
@@ -3,14 +3,16 @@ const throwing_knife = preload("res://Throwing_Knife.tscn")
 
 var direction: Vector2 = Vector2.ZERO
 @export var speed: int = 50
+var knife_ready = false
 
 @onready var sprite = $PlayerSprite
 @onready var main = get_node('/root/Main')
 
 func _process(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
-	if Input.is_action_just_pressed("attack"):
+	if Input.is_action_pressed("attack") and knife_ready == true:
 		knife()
+		knife_ready = false
 
 func _physics_process(delta):
 	velocity = direction * speed
@@ -29,6 +31,8 @@ func knife():
 	knife.rotate(global_position.direction_to(get_global_mouse_position()).angle())
 	main.add_child(knife)
 	
-	
+func _on_timer_timeout():
+	knife_ready = true
 
-	
+
+
