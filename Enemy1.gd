444d8071f2dc1024b831e66b8ddf46067fb6910f
guywiththenose <sammylemonmoore@gmail.com>
2024-08-14 12:26:13 +1200

    v1.24

--- Enemy1.gd
+++ Enemy1.gd
@@ -7,6 +7,7 @@ extends CharacterBody2D
 @export var health = 2
 @onready var damagetimer = $Hitbox/Damagetimer
 @export var damage = 5
+@onready var navigation_agent := $NavigationAgent2D as NavigationAgent2D
 var moveable = false
 
 func check_collision():
@@ -19,18 +20,17 @@ func check_collision():
 				PlayerStats.damage_player(damage)
 				damagetimer.start()
 
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	if moveable == true:
-		var direction_to_player = global_position.direction_to(player.global_position)
-		velocity = direction_to_player * speed
+		var dir = to_local(navigation_agent.get_next_path_position()).normalized()
+		velocity = dir * speed
+		if velocity.x > 0:
+			sprite.flip_h = true
+		else:
+			sprite.flip_h = false
 	
-	if velocity.x > 0:
-		sprite.flip_h = true
-	else:
-		sprite.flip_h = false
-	
-	move_and_slide()
-	check_collision()
+		move_and_slide()
+		check_collision()
 
 func take_damage(dmg):
 	health -= dmg
@@ -38,6 +38,15 @@ func take_damage(dmg):
 		queue_free()
 
 
+
 func _on_playerbox_body_entered(body):
 	if body.is_in_group("player"):
 		moveable = true
+
+func make_path() -> void:
+	navigation_agent.target_position = player.global_position
+
+
+
+func _on_nav_timer_timeout():
+	make_path()
