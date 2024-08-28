f91e7d6530154cc80cf6684842b7b230682354e6
guywiththenose <sammylemonmoore@gmail.com>
2024-08-29 11:00:34 +1200

    v1.32
    miaow

---
+++ Wait.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_start_pressed():
+	
+	get_tree().change_scene_to_file("res://Tilemap/main.tscn")
+	PlayerStats.player_health = 100
